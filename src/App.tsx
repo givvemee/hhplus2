2a255da5a87b0533dedfe4e3256d8c5692a0ea9e
givvemee <givvemeee@gmail.com>
2024-04-02 22:26:12 +0900

    added dependency, set meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={'it is a description'} />
+        <meta property="og:title" content={'a title'} />
+        <meta property="og:description" content={'it is a description'} />
+        <meta property="og:url" content={'https://www.vivi.page/'} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:title" content={'a title'} />
+        <meta name="twitter:description" content={'it is a description'} />
+        <title>Title</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
